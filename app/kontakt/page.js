f727b7911b1cdcc7db3cae462e757afe4270dbad
Helena Adelsten <helenaltv@gmail.com>
2025-04-29 10:37:07 +0200

    changes

--- app/kontakt/page.js
+++ app/kontakt/page.js
@@ -1,10 +1,72 @@
-import { Suspense } from "react";
-import FormComponent from "@/app/components/FormComponent";
+"use client";
 
-export default function Page() {
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import styles from "./index.module.css";
+
+const KontaktSida = () => {
+  const [state, handleSubmit] = useForm("mannvbln");
+
+  if (state.succeeded) {
+    return (
+      <div className={styles.successMessage}>
+        <h1 className={styles.kontakth1}>Tack för din förfrågan!</h1>
+        <p>Vi kommer att kontakta dig så snart som möjligt.</p>
+      </div>
+    );
+  }
+
   return (
-    <Suspense fallback={<div>Laddar formulär...</div>}>
-      <FormComponent />
-    </Suspense>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Kontakta oss</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.inputGroup}>
+          <label htmlFor="name">Namn</label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Ditt namn"
+            required
+          />
+          <ValidationError prefix="Name" field="name" errors={state.errors} />
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="email">E-postadress</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Din e-postadress"
+            required
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="message">Meddelande</label>
+          <textarea
+            id="message"
+            name="message"
+            rows="5"
+            placeholder="Ditt meddelande"
+            required
+          ></textarea>
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
+        <button
+          type="submit"
+          className={styles.inputButton}
+          disabled={state.submitting}
+        >
+          {state.submitting ? "Skickar..." : "Skicka"}
+        </button>
+      </form>
+    </div>
   );
-}
+};
+
+export default KontaktSida;
